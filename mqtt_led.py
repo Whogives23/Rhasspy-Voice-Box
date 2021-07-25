7b0da7cbbc5cccd611effac86840fed16e0cbb0f
WhoGives23 <sjleroux23@gmail.com>
2021-07-25 15:25:44 +0100

    Variabalizing MQTT settings

--- mqtt_led.py
+++ mqtt_led.py
@@ -4,8 +4,8 @@ from rpi_ws281x import PixelStrip, Color
 import argparse
 
 # LED strip configuration:
-LED_COUNT = 16        # Number of LED pixels.
-LED_PIN = 12          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_COUNT = <LEDCount>        # Number of LED pixels.
+LED_PIN = <LEDPin>          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
@@ -13,12 +13,12 @@ LED_INVERT = False    # True to invert the signal (when using NPN transistor lev
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 #MQTT Broker Info
-broker = '192.168.1.8'
+broker = '<MQTTHost>'
 port = 1883
-topic = "/oldtimerled/mqtt"
-client_id = 'python-mqtt-oldtimer'
-username = 'BoB'
-password = '118H@igR0ad'
+topic = "/<SiteId>/mqtt"
+client_id = 'python-mqtt-<SiteId>'
+username = '<MQTTUsername>'
+password = '<MQTTPassword>'
 
 #Topics to subscribe to
 hotwordTopic = "hermes/hotword/jarvis_raspberry-pi/detected"
